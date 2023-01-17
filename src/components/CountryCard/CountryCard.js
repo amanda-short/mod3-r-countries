a9b42bf1f8feb5ed56ccc141f27f45937a5ce340
amanda-short <AmandaShort@Kamies-MacBook-Pro.local>
2023-01-17 15:44:53 -0800

    flag images linked and appearing

--- src/components/CountryCard/CountryCard.js
+++ src/components/CountryCard/CountryCard.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import './CountryCard.css';
 
-export default function CountryCard({ name, image }) {
+export default function CountryCard({ name, iso2 }) {
   return (
     <div className="card">
-      <h2>{name}</h2>
-      <img alt={name} height="100px" src={image} />
+      <h5>{name}</h5>
+      <img alt={name} height="100px" src={`https://flagcdn.com/w20/${iso2.toLowerCase()}.png`} />
     </div>
   );
 }
